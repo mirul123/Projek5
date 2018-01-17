f8cfed2121b39b7160ce3ab8950c2d5b39edfe9d
Muhammad Muizuddin <Muhammad Muizuddin@MuhammadMuizuddin-LAPTOP-JO7D1JT3>
2018-01-17 18:37:55 +0800

    Commit version 1

---
+++ src/Pekerjaan.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.Scanner;
+/**
+ *
+ * @author Muhammad Muizuddin
+ */
+public class Pekerjaan {
+    public static void main(String[]args){
+        Scanner input=new Scanner(System.in);
+        
+        String nama,kerja;
+        double gaji;
+        
+    System.out.println("Masukkan nama anda: ");
+    nama = input.nextLine();
+    
+    System.out.println("Masukkan pekerjaan anda: ");
+    kerja = input.nextLine();
+    
+    System.out.println("Masukkan gaji anda: ");
+    gaji = input.nextDouble();
+    
+    System.out.println("============================================");
+    System.out.println("BIODATA PEKERJA");
+    System.out.println("NAMA: "+nama);
+    System.out.println("GAJI: "+gaji);
+    System.out.println("PEKERJAAN: "+kerja);
+    
+    }
+    
+}
